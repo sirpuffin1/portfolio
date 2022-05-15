957270f8905a82257196632310bd9307d1f9c0eb
Angel <angelcruz981@yahoo.com>
2022-05-15 13:58:13 -0700

    ref(prisma.js): change const to let

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+let prisma = new PrismaClient()
 
 if(process.env.NODE_ENV === 'production'){
     prisma = new PrismaClient()
