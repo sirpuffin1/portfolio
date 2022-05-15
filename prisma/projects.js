879ef81e4e294f075b4ad56e9b6e5b7fe5fb8b1e
Angel <angelcruz981@yahoo.com>
2022-05-15 13:23:26 -0700

    feat(projects.js): add function to get a project by id

--- prisma/projects.js
+++ prisma/projects.js
@@ -4,3 +4,10 @@ export const getAllProjects = async () => {
     const projects = await prisma.projects.findMany({})
     return projects
 }
+
+export const getProject = async id => {
+    const project = await prisma.projects.findUnique({
+        where: { id }
+    })
+    return project 
+}
