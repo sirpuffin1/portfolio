73c2934c072cf82f83e5cca1570fe535d99e1a8b
Angel <angelcruz981@yahoo.com>
2022-05-15 14:57:53 -0700

    chore(app): import motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { useEffect } from "react"
 import Navbar from '../components/Navbar';
+import { motion } from 'framer-motion'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
