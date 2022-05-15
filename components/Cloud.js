341c1af1851d0793a8cf3e2464838486cd75a904
Angel <angelcruz981@yahoo.com>
2022-05-15 14:55:31 -0700

    feat(Cloud): create cloud svg

--- components/Cloud.js
+++ components/Cloud.js
@@ -1,8 +1,21 @@
-const Cloud = () => {
+const Cloud = (props) => {
     return (
-        <div>
-            Enter
-        </div>
+        <div >
+<svg  viewBox='0 0 105 105' width="60" height="60">
+  
+  <path d='M 25,60 
+           a 20,20 1 0,0 0,40 
+           h 50 
+           a 20,20 1 0,0 0,-40 
+           a 10,10 1 0,0 -15,-10 
+           a 15,15 1 0,0 -35,10  
+           z' fill="#60A5FA" stroke="black" strokeWidth="5"/>
+           <a href="https://github.com/">
+           <text x="50%" y="69%" fill="white" alignmentBaseline="hanging" textAnchor="middle">{props.text}</text>
+           </a>
+           
+</svg>
+</div>
     );
 }
 
