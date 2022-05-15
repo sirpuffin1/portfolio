2e5c3b3258b54b280f42e9fa4b0c78a14ee7c482
Angel <angelcruz981@yahoo.com>
2022-05-15 14:10:51 -0700

    chore(devIcon): configure app.js to use cdn

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const devIconLink = document.createElement("link");
+    devIconLink.setAttribute("id", "devIconLink");
+    devIconLink.rel = 'stylesheet';
+    devIconLink.href = 'https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css'
+    document.getElementsByTagName("head")[0].appendChild(devIconLink);
+    return () => {
+      if (devIconLink) {
+        devIconLink.remove();
+      }
+    };
+  }, []);
   return <Component {...pageProps} />
 }
 
