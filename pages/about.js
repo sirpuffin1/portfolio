37fb00ff306eb5e09ddf0760d1bad542238572be
Angel <angelcruz981@yahoo.com>
2022-05-16 15:20:29 -0700

    feat(about): create about me page with personal info

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,16 @@
 const about = () => {
     return (
-        <div>
-            about
+        <div className="bg-blue-200 h-screen">
+        <h1 className="text-9xl font-bold text-white">About Me</h1>
+        <div className="p-4 overflow-hidden bg-transparent w-full text-center rounded-lg border-8 border-black shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center content-center" >
+       
+        
+        <p className="font-bold text-3xl text-blue-900">A MEAN stack devloper who is constantly diving into new techonology and tries their best to find the next cool thing. My favorite thing about development is finding new things and mashing them together until I can get them to work. </p>
+        <p className="font-bold text-3xl text-blue-900">I currently work for Bitwise Industries as a MEAN stack apprentice. This opportunity has allowed me to work under a lead developer and learn as much as possible. I feel like I&apos;ve learned a lot in my time with this company and I&apos;m ready to branch away and see what&apos;s out there.</p>
+        <p className="font-bold text-3xl text-blue-600">The sky&apos;s the limit.</p>
+        
+        
+        </div>
         </div>
     );
 }
