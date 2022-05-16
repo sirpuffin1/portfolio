039cef9a28117fae0ffb9192672f93389c3852bf
Angel <angelcruz981@yahoo.com>
2022-05-16 15:25:51 -0700

    feat(card): added styling

--- components/Card.js
+++ components/Card.js
@@ -5,15 +5,15 @@ const Card = (props) => {
     const gitHub = "GitHub"
     return (
         <div className={styles.floating}>
-            <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <div className="max-w-sm bg-sky-200 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
         <img className="rounded-t-lg" src={props.imgUrl} alt="" />
     </a>
     <div className="p-5">
         <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.title}</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-sky-700 dark:text-white">{props.title}</h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 border-b-2 border-b-indigo-500">{props.description}</p>
+        <p className="mb-3 font-bold text-gray-700 dark:text-gray-400 border-b-2 border-b-indigo-500">{props.description}</p>
         <div>
             {props.tools.map((tool) => (
                 <ul key={tool} className="inline-flex  ">
