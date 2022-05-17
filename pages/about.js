b5b71290d059dc7aca0593b092613afcb3631005
Angel <angelcruz981@yahoo.com>
2022-05-17 13:07:01 -0700

    ref(about): fix typo in technology

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ const about = () => {
         <div className="p-4 overflow-hidden  text-center rounded-lg  shadow-lg sm:p-8 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center content-center m-8" >
        
         
-        <p className="font-bold text-3xl text-blue-900 mb-4">A MEAN stack devloper who is constantly diving into new techonology and tries their best to find the next cool thing. My favorite thing about development is finding new things and mashing them together until I can get them to work. </p>
+        <p className="font-bold text-3xl text-blue-900 mb-4">A MEAN stack devloper who is constantly diving into new technology and tries their best to find the next cool thing. My favorite thing about development is finding new things and mashing them together until I can get them to work. </p>
         <p className="font-bold text-3xl text-blue-900 mb-4">I currently work for Bitwise Industries as a MEAN stack apprentice. This opportunity has allowed me to work under a lead developer and learn as much as possible. I feel like I&apos;ve learned a lot in my time with this company and I&apos;m ready to branch away and see what&apos;s out there.</p>
         <p className="font-bold text-3xl text-blue-600 mb-4">The sky&apos;s the limit.</p>
         
