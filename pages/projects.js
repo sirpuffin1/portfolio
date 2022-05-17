ac98a550eb48c89e02c2100a5d74ea82c4cdf87e
Angel <angelcruz981@yahoo.com>
2022-05-16 17:51:13 -0700

    ref(projects): remove classNames

--- pages/projects.js
+++ pages/projects.js
@@ -15,13 +15,15 @@ export const getStaticProps = async ({ req }) => {
 const projects = ({projects}) => {
     
     return (
-        <div className="bg-blue-200 h-screen">
-            <h1 className="text-9xl font-bold text-white">Projects</h1>
-            <div className="p-10 m-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-x-20">
+        <div>
+        <div >
+            <h1 className="text-7xl font-bold text-white">Projects</h1>
+            <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5">
             {projects.map(project => (
                 <Card key={project.title} description={project.description} title={project.title} imgUrl={project.imgUrl} tools={project.tools} gitHubLink={project.gitHubLink} liveSiteLink={project.liveSiteLink}/>
             ))}
             </div>
+        </div>
         </div>
     );
 }
